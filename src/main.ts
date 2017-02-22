aa4ee9358baeae195d36ade8d22d2289dbba335d
ulflunde <ulf.lunde@gmail.com>
2017-02-22 14:04:35 +0100

    Initial commit from Git Gui

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+/* Cloned from https://github.com/angular/quickstart */
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
