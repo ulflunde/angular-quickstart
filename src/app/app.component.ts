1d9c67ab8611c5b386fa3485e7b81f0dd907389a
ulflunde <ulf.lunde@gmail.com>
2017-02-22 15:32:28 +0100

    No change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>Hello {{name}}</h1>My name is Ulf.`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { name = "World"; }
