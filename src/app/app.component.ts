4d68a6878e36cfaa5cb4255080caaa9381903025
ulflunde <ulf.lunde@gmail.com>
2017-02-23 14:40:52 +0100

    Renamed custom tags.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'ulfs-app',
   template: `<h1>Hello {{name}}</h1>My name is Ulf.`,
 })
 export class AppComponent  { name = "World"; }
